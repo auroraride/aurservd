e5c6bea457761280238ff4e4db12e14d2e2c38cf
liasica <magicrolan@qq.com>
2021-12-10 18:22:49 +0800

    其他辅助

--- internal/ent/internal/mixin.go
+++ internal/ent/internal/mixin.go
@@ -4,3 +4,31 @@
 // Based on aurservd by liasica, magicrolan@qq.com.
 
 package internal
+
+import (
+    "entgo.io/ent"
+    "entgo.io/ent/schema/field"
+    "entgo.io/ent/schema/mixin"
+    "time"
+)
+
+type TimeMixin struct {
+    mixin.Schema
+}
+
+func (TimeMixin) Fields() []ent.Field {
+    return []ent.Field{
+        field.Time("created_at").Immutable().Default(time.Now),
+        field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
+    }
+}
+
+type DeleteMixin struct {
+    mixin.Schema
+}
+
+func (DeleteMixin) Fields() []ent.Field {
+    return []ent.Field{
+        field.Time("deleted_at").Nillable().Optional(),
+    }
+}
--- pkg/utils/string.go
+++ pkg/utils/string.go
@@ -5,7 +5,16 @@
 
 package utils
 
-import "unicode"
+import (
+    "regexp"
+    "strings"
+    "unicode"
+)
+
+var (
+    matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+    matchAllCap   = regexp.MustCompile("([a-z0-9])([A-Z])")
+)
 
 func StrToFirstUpper(str string) string {
     if len(str) == 0 {
@@ -14,4 +23,10 @@ func StrToFirstUpper(str string) string {
     tmp := []rune(str)
     tmp[0] = unicode.ToUpper(tmp[0])
     return string(tmp)
+}
+
+func StrToSnakeCase(str string) string {
+    snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+    snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+    return strings.ToLower(snake)
 }
