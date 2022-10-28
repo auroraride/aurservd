9566eb5aea70c31f6811b4a110bbeede42eb9cf2
liasica <magicrolan@qq.com>
2022-10-28 14:11:02 +0800

    修复合同数据

--- cmd/aurservd/internal/script/fix/contract.go
+++ cmd/aurservd/internal/script/fix/contract.go
@@ -11,6 +11,8 @@ import (
     "github.com/auroraride/aurservd/app/model"
     "github.com/auroraride/aurservd/internal/ent"
     "github.com/auroraride/aurservd/internal/esign"
+    "github.com/auroraride/aurservd/pkg/snag"
+    log "github.com/sirupsen/logrus"
     "github.com/spf13/cobra"
     "time"
 )
@@ -21,27 +23,53 @@ func Contract() *cobra.Command {
         Short: "修复签约时间",
         Run: func(cmd *cobra.Command, args []string) {
             ctx := context.Background()
-            items, _ := ent.Database.Contract.QueryNotDeleted().All(ctx)
-            fmt.Printf("即将查询 %d 条结果\n", len(items))
+            items, _ := ent.Database.Contract.QueryNotDeleted().WithRider().All(ctx)
+            log.Printf("即将查询 %d 条结果", len(items))
+            s := esign.New()
             for _, item := range items {
-                fmt.Printf("查询 %d 签约时间\n", item.ID)
-                status, sr := esign.New().Result(item.FlowID)
-                at, ok := sr.SignValidity.(float64)
+                err := snag.WithPanic(func() {
 
-                updater := item.Update()
+                    log.Printf("查询 %d 签约时间", item.ID)
+                    status, sr := s.Result(item.FlowID)
 
-                // 如果签约完成但是签约完成时间为空
-                if item.SignedAt == nil && status == model.ContractStatusSuccess {
-                    updater.SetSignedAt(sr.EndAt())
+                    updater := item.Update()
+
+                    // 设置骑手
+                    r := item.Edges.Rider
+                    if r != nil {
+                        updater.SetRiderInfo(&model.ContractRider{
+                            Phone:        r.Phone,
+                            Name:         r.Name,
+                            IDCardNumber: r.IDCardNumber,
+                        })
+                    }
+
+                    // 修复状态
+                    if status == model.ContractStatusSuccess {
+                        updater.SetSignedAt(sr.EndAt()).SetStatus(model.ContractStatusSuccess.Value())
+                        // 文档修复, 查询并下载文档
+                        if len(item.Files) == 0 {
+                            updater.SetFiles(s.DownloadDocument(fmt.Sprintf("%s-%s/contracts/", r.Name, r.IDCardNumber), item.FlowID))
+                        }
+                    } else {
+                        // 设置过期时间
+                        if at, ok := sr.SignValidity.(float64); ok {
+                            t := time.UnixMilli(int64(at))
+                            updater.SetExpiresAt(t)
+                        }
+                        if at, ok := sr.ContractValidity.(float64); ok {
+                            t := time.UnixMilli(int64(at))
+                            updater.SetExpiresAt(t)
+                        }
+                    }
+
+                    _ = updater.Exec(ctx)
+                })
+
+                if err != nil {
+                    log.Error(err)
                 }
 
-                // 如果过期时间为空
-                if item.ExpiresAt == nil && ok {
-                    t := time.UnixMilli(int64(at))
-                    updater.SetExpiresAt(t)
-                }
-
-                _ = updater.Exec(ctx)
                 time.Sleep(1 * time.Second)
             }
         },
