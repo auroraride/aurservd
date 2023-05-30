78b92191b18f8e17bfef7315e4a70e3667e96034
liasica <magicrolan@qq.com>
2023-05-30 09:32:48 +0800

    修复数据校验bug

--- app/model/ebike_brand.go
+++ app/model/ebike_brand.go
@@ -12,8 +12,8 @@ type EbikeBrand struct {
 }
 
 type EbikeBrandCreateReq struct {
-	Name  string `json:"name" validate:"required" validate:"名称"`   // 名称
-	Cover string `json:"cover" validate:"required" validate:"封面图"` // 封面图
+	Name  string `json:"name" validate:"required"`  // 名称
+	Cover string `json:"cover" validate:"required"` // 封面图
 }
 
 type EbikeBrandModifyReq struct {
