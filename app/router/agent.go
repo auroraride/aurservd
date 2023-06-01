1dc65e0267385297fa47752a8c361105f3572487
liasica <magicrolan@qq.com>
2023-06-01 08:50:55 +0800

    删除残留路由

--- app/router/agent.go
+++ app/router/agent.go
@@ -24,10 +24,6 @@ func loadAgentRoutes() {
 	auth.GET("/openid", aapi.Agent.GetOpenid)
 	// 添加骑手
 	auth.POST("/rider", aapi.Rider.Create)
-	// 站点列表
-	auth.GET("/site/list", aapi.Agent.SiteList)
-	// 城市列表
-	auth.GET("/city/list", aapi.Agent.CityList)
 	// 电池搜索列表
 	auth.GET("/battery/list", aapi.Agent.BatteryList)
 	// 骑手列表
