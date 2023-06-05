1681e25d9d8313c9fd96127c9a98e3d9d859efec
liasica <magicrolan@qq.com>
2023-06-05 17:09:41 +0800

    doc生成bug修复

--- generate_doc.sh
+++ generate_doc.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/usr/local/go/bin
 GOMODCACHE=$(go env GOMODCACHE)
 
 dir() {
-  echo "${GOMODCACHE}/$1@$(sed <go.mod $'/^require ($/,/^)$/!d; /^require ($/d;/^)$/d; /\\/\\/ indirect$/d; s/^\t+//g' | grep "$1" | cut -d' ' -f2)"
+  echo "${GOMODCACHE}/$1@$(< go.mod sed $'/^require ($/,/^)$/!d; /^require ($/d;/^)$/d; /\\/\\/ indirect$/d; s/^\t+//g' | grep "$1" | cut -d' ' -f2)"
   return 0
 }
 
