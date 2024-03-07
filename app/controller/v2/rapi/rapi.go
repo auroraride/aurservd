274f0dea20fe5755d2c83b09273305cf5e6b4d8c
lisicen <kamijoucen@foxmail.com>
2024-03-07 11:31:59 +0800

    调整代码格式

--- app/controller/v2/rapi/rapi.go
+++ app/controller/v2/rapi/rapi.go
@@ -5,30 +5,6 @@
 
 package rapi
 
-import (
-	"github.com/auroraride/aurservd/app"
-	"github.com/auroraride/aurservd/app/biz"
-	"github.com/auroraride/aurservd/app/model"
-	"github.com/labstack/echo/v4"
-)
-
 // @title		极光出行API - 骑手端api
 // @version		2.0
 // @BasePath	/
-
-var Rider = new(rider)
-
-// Feedback
-// @ID		RiderFeedback
-// @Router	/rider/v2/feedback [POST]
-// @Summary	AZ001 意见反馈
-// @Tags	[R]骑手接口
-// @Accept	json
-// @Produce	json
-// @Param	X-Rider-Token	header		string				true	"骑手校验token"
-// @Param	body			body		model.FeedbackReq	true	"反馈内容"
-// @Success	200				{object}	bool				"请求成功"
-func (*rider) Feedback(c echo.Context) (err error) {
-	ctx, req := app.RiderContextAndBinding[model.FeedbackReq](c)
-	return ctx.SendResponse(biz.NewFeedback().RiderCreate(req, ctx.Rider))
-}
